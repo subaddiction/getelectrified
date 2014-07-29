3434716be968ae65e5277620dc6e392f2d15cebf
mrk25 <subaddiction@subaddiction.net>
2014-07-29 19:23:58 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+
+
+function parseFeed($url, $max_items=8, $tagname='item'){
+                $doc = new DOMDocument();
+                $doc->load($url);
+                $feed = array();
+                $items = $doc->getElementsByTagName($tagname);
+                $nodeNUM = 0;
+                foreach ($items as $item) {
+                    //echo $item->nodeValue . "<br />";
+                    $feed[$nodeNUM] = array();
+                    foreach($item->childNodes as $node){
+                        $feed[$nodeNUM][$node->nodeName] = $node->nodeValue;
+                    }
+                    $nodeNUM++;
+                    if($nodeNUM >= $max_items){ break; }
+                }
+                return $feed;
+}
+
+
+
+function mixArrays($arrays=array(), $orderField, $order='desc',
+$strtotime=false){
+	
+	$result = array();
+	foreach($arrays as $ka => $va){
+	
+		foreach($va as $k => $v){
+			if($strtotime){
+			$result[strtotime($v[$orderField])] = $v;
+			} else {
+			$result[$v[$orderField]] = $v;
+			}
+			
+			//print_r($v);
+		
+		}
+	
+	}
+	
+	switch($order){
+		case 'desc':
+			krsort($result);
+		break;
+		
+		default:
+			ksort($result);
+		
+	}
+	
+	return $result;
+
+}
+
+function arrayRemap($inputArray=array(), $fieldsMap=array()){
+	
+	foreach($inputArray as $ka=>$va){
+		foreach($fieldsMap as $k=>$v){
+			
+			$inputArray[$ka][$v] = $inputArray[$ka][$k];
+		
+		}
+	}
+	
+	return $inputArray;
+
+}
+
+
+if(file_exists('index.json')){
+	$update = (time() - filemtime('index.json') >= (60*60*24))?true:false; // index.json modificato da > di 24h
+}
+
+if(!file_exists('index.json') || $update){
+
+	$blogs = array(
+		parseFeed('http://subaddiction.net/?feed=rss2'),
+		parseFeed('http://electroswingitalia.com/?feed=rss2'),
+		parseFeed('http://wobblerecords.com/?feed=rss2'),
+		parseFeed('http://wobblelovers.com/?feed=rss2'),
+		parseFeed('http://glitchhop.it/?feed=rss2'),
+	);
+
+	$test = mixArrays($blogs, 'pubDate', 'desc', true);
+
+	$mix = array();
+	$i = 0;
+	foreach($test as $kk=>$vv){
+		$i++;
+		if($i > 8){
+			break;
+		} else {
+			//echo $kk." --- ".$vv['pubDate']." --- ".$vv['title']."\n";
+			$mix[] = $vv;
+		}
+	
+	}
+	
+	$mix = json_encode($mix);
+	@file_put_contents('index.json', $mix);
+
+}
+
+
+include('tmpl.php');
+
+
+?>
+
+
---
+++ tmpl.php
@@ -0,0 +1,52 @@
+<?php
+
+$debug = ($_GET['json'])?true:false;
+$fullposts = false;
+
+
+if($debug){ 
+	header('content-type:application/json');
+	include('index.json');
+	die();
+} else {
+	$contents = json_decode(@file_get_contents('index.json'),true); 
+}
+
+?><!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Get Electrified</title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="author" content="mrk25" />
+<meta name="robots" content="index, follow" />
+<meta name="viewport" content="width=device-width" />
+<link rel="shortcut icon" href="favicon.ico" />
+<? /* <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/bootstrap-theme.min.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/fonts.css" type="text/css" media="screen" /> */ ?>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<? /* <script type="text/javascript" src="js/jquery-2.0.3.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/frontend.js"></script>  */ ?>
+</head>
+<body>
+<header>
+<h1>Get Electrified Blog</h1>
+<h2>Musica elettronica, DJing, audio &amp; video live set.</h2>
+</header>
+<?php foreach($contents as $post){ ?>
+	<div>
+		<h2 title="<?php echo $post['title']; ?>">
+			<a href="<?php echo $post['link']; ?>">
+				<?php echo $post['title']; ?>
+			</a>
+		</h2>
+		<div>
+			<?php echo ($fullposts)?$post['content:encoded']:$post['description']; ?>
+		</div>
+	</div>
+<?php } ?>
+</body>
+</html>
